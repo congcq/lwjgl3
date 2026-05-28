645dfdde8c72ef80bc7f735a01c9f466e259950d
CongChu <aiskai2007@gmail.com>
2026-05-28 22:14:50 +0700

    Update Sys.java

--- modules/lwjgl/lwjglx/src/main/java/org/lwjgl/Sys.java
+++ modules/lwjgl/lwjglx/src/main/java/org/lwjgl/Sys.java
@@ -1,6 +1,7 @@
 package org.lwjgl;
 
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.system.Platform;
 import org.lwjgl.glfw.GLFW;
 
 import java.awt.Desktop;
@@ -11,10 +12,17 @@ import javax.swing.UIManager;
 
 public class Sys {
     
-    /**
-     * No constructor for Sys.
-     */
     private Sys() {
+        try {
+            // pojavexec is used to bridge GLFW with Android/iOS and loading vulkan driver for Android.
+            if (Platform.get() == Platform.MACOSX) {
+                System.load(System.getenv("BUNDLE_PATH") + "/PojavPatch");
+            } else if (Platform.get() == Platform.LINUX) {
+                System.loadLibrary("pojavexec");
+            }
+        } catch (UnsatisfiedLinkError e) {
+            e.printStackTrace();
+        }
     }
 
     /** Returns the LWJGL version. */
