0a4553e87d394a368cd88087d59297775c205f59
CongChu <aiskai2007@gmail.com>
2026-08-02 18:28:24 +0700

    don't need this

--- modules/lwjgl/core/src/main/java/org/lwjgl/system/Library.java
+++ modules/lwjgl/core/src/main/java/org/lwjgl/system/Library.java
@@ -55,16 +55,6 @@ public final class Library {
             );
         }
 
-        try {
-            // pojavexec is used to bridge GLFW with Android/iOS and loading vulkan driver for Android.
-            if (Platform.get() == Platform.MACOSX) {
-                System.load(System.getenv("BUNDLE_PATH") + "/Hynis");
-            } else if (Platform.get() == Platform.LINUX) {
-                System.loadLibrary("pojavexec");
-            }
-        } catch (UnsatisfiedLinkError e) {
-            e.printStackTrace();
-        }
         loadSystem("org.lwjgl", JNI_LIBRARY_NAME);
     }
 
